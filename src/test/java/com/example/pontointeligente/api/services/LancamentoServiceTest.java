d8dafe03db43475d78e344b086c05b42c9306a0a
Danilo Costa <daniloc@ciandt.com>
2018-10-14 10:26:32 -0300

    alterando profile de testes

--- src/test/java/com/example/pontointeligente/api/services/LancamentoServiceTest.java
+++ src/test/java/com/example/pontointeligente/api/services/LancamentoServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.devtools.autoconfigure.OptionalLiveReloadServer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
